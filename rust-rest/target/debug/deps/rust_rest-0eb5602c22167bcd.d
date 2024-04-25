57e736976ee82461f185b191552b0f7bfff6752d
marvleon <marvleon@pdx.edu>
2024-04-24 21:09:06 -0700

    debug

---
+++ rust-rest/target/debug/deps/rust_rest-0eb5602c22167bcd.d
@@ -0,0 +1,5 @@
+/Users/marvleon/code/Rust-Practice/rust-rest/target/debug/deps/librust_rest-0eb5602c22167bcd.rmeta: src/main.rs
+
+/Users/marvleon/code/Rust-Practice/rust-rest/target/debug/deps/rust_rest-0eb5602c22167bcd.d: src/main.rs
+
+src/main.rs:
