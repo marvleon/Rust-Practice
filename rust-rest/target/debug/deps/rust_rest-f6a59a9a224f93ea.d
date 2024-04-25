9b8be1061e2c027d972782134e114350ddff5733
marvleon <marvleon@pdx.edu>
2024-04-24 19:50:27 -0700

    develop question type

---
+++ rust-rest/target/debug/deps/rust_rest-f6a59a9a224f93ea.d
@@ -0,0 +1,9 @@
+C:\Users\marvin\code\rust-cs410\Rust-Practice\rust-rest\target\debug\deps\librust_rest-f6a59a9a224f93ea.rmeta: src\main.rs Cargo.toml
+
+C:\Users\marvin\code\rust-cs410\Rust-Practice\rust-rest\target\debug\deps\rust_rest-f6a59a9a224f93ea.d: src\main.rs Cargo.toml
+
+src\main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
---
+++ rust-rest/target/debug/deps/rust_rest.d
@@ -0,0 +1,5 @@
+C:\Users\marvin\code\rust-cs410\Rust-Practice\rust-rest\target\debug\deps\rust_rest.exe: src\main.rs
+
+C:\Users\marvin\code\rust-cs410\Rust-Practice\rust-rest\target\debug\deps\rust_rest.d: src\main.rs
+
+src\main.rs:
