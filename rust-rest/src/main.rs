b02c90fc4e2f83e77b7c610bab0565a500c68d70
marvleon <marvleon@pdx.edu>
2024-04-25 16:07:45 -0700

    add comments and axum

--- rust-rest/src/main.rs
+++ rust-rest/src/main.rs
@@ -1,6 +1,10 @@
+use axum::{routing::get, Router};
 use std::io::{Error, ErrorKind};
 use std::str::FromStr;
 
+//Custom parsing for converting string to QuestionId
+//For extracting QuestionId from strings (user input or data field)
+//Returns QuestionId if string is not empty, error if empty
 impl FromStr for QuestionId {
     type Err = std::io::Error;
     fn from_str(id: &str) -> Result<Self, Self::Err> {
@@ -22,6 +26,8 @@ struct Question {
 #[derive(Debug)]
 struct QuestionId(String);
 
+//Func for new instance of Question
+//Takes ownership of input parameters and puts them to respective properties of Question struct
 impl Question {
     fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
         Question {
@@ -33,16 +39,15 @@ impl Question {
     }
 }
 
-fn main() {
-    let question = Question::new(
-        QuestionId::from_str("1").expect("No id provided"),
-        "First Question".to_string(),
-        "Content of question".to_string(),
-        Some(vec!["faq".to_string()]),
-    );
-    println!("{:?}", question);
+#[tokio::main]
+async fn main() {
+    let hello = axum::get().map(|| format!("Hello, World!!"));
+    axum::serve(hello).run(([127, 0, 0, 1], 3030)).await;
 }
 
+//Implementation of Dipslay trait for struct Question
+//Enables custom string formatting of Question instances
+//Displays the question struct in a formatted string
 impl std::fmt::Display for Question {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
         write!(
@@ -53,6 +58,9 @@ impl std::fmt::Display for Question {
     }
 }
 
+//Implementation of Display trait for struct QuestionId
+//Allows custom string formatting for QuestionId type
+//Outputs the id of the QuestionId (printed or logged)
 impl std::fmt::Display for QuestionId {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
         write!(f, "id: {}", self.0)
