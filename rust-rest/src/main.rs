51d8f82fafc9c58812ed80e5c8345c31ce88c209
marvleon <marvleon@pdx.edu>
2024-04-26 01:40:05 -0700

    move question struct into file

--- rust-rest/src/main.rs
+++ rust-rest/src/main.rs
@@ -1,50 +1,13 @@
+mod question;
+use question::*;
+
 use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
     routing::get,
     Json, Router,
 };
-use serde::Serialize;
-use std::io::{Error, ErrorKind};
 use std::net::SocketAddr;
-use std::str::FromStr;
-
-//Custom parsing for converting string to QuestionId
-//For extracting QuestionId from strings (user input or data field)
-//Returns QuestionId if string is not empty, error if empty
-impl FromStr for QuestionId {
-    type Err = std::io::Error;
-    fn from_str(id: &str) -> Result<Self, Self::Err> {
-        match id.is_empty() {
-            false => Ok(QuestionId(id.to_string())),
-            true => Err(Error::new(ErrorKind::InvalidInput, "No id provided")),
-        }
-    }
-}
-
-#[derive(Debug, Serialize)]
-struct Question {
-    id: QuestionId,
-    title: String,
-    content: String,
-    tags: Option<Vec<String>>,
-}
-
-#[derive(Debug, Serialize)]
-struct QuestionId(String);
-
-//Func for new instance of Question
-//Takes ownership of input parameters and puts them to respective properties of Question struct
-impl Question {
-    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
-        Question {
-            id,
-            title,
-            content,
-            tags,
-        }
-    }
-}
 
 #[tokio::main]
 async fn main() {
@@ -63,9 +26,9 @@ async fn main() {
 
 async fn get_questions() -> impl IntoResponse {
     let question = Question::new(
-        QuestionId::from_str("1").expect("No id provided"),
-        "First Questions".to_string(),
-        "Content of question".to_string(),
+        1,
+        "First Questions",
+        "Content of question",
         Some(vec!["faq".to_string()]),
     );
     Json(question) //Return statement 200 OK with JSON serialized data
@@ -73,25 +36,4 @@ async fn get_questions() -> impl IntoResponse {
 
 async fn handler_not_found() -> Response {
     (StatusCode::NOT_FOUND, "404 Not Found").into_response()
-}
-//Implementation of Dipslay trait for struct Question
-//Enables custom string formatting of Question instances
-//Displays the question struct in a formatted string
-impl std::fmt::Display for Question {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
-        write!(
-            f,
-            "{}, title: {}, content: {}, tags: {:?}",
-            self.id, self.title, self.content, self.tags
-        )
-    }
-}
-
-//Implementation of Display trait for struct QuestionId
-//Allows custom string formatting for QuestionId type
-//Outputs the id of the QuestionId (printed or logged)
-impl std::fmt::Display for QuestionId {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
-        write!(f, "id: {}", self.0)
-    }
 }
---
+++ rust-rest/src/question.rs
@@ -0,0 +1,64 @@
+use serde::Serialize;
+use std::io::{Error, ErrorKind};
+use std::str::FromStr;
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize)]
+pub struct QuestionId(String);
+
+#[derive(Debug, Clone, Serialize)]
+pub struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+
+//Func for new instance of Question
+//Takes ownership of input parameters and puts them to respective properties of Question struct
+impl Question {
+    pub fn new(id: usize, title: &str, content: &str, tags: Option<Vec<String>>) -> Self {
+        let id = QuestionId(id.to_string());
+        let title = title.into();
+        let content = content.into();
+        Self {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+//Custom parsing for converting string to QuestionId
+//For extracting QuestionId from strings (user input or data field)
+//Returns QuestionId if string is not empty, error if empty
+impl FromStr for QuestionId {
+    type Err = std::io::Error;
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(Error::new(ErrorKind::InvalidInput, "No id provided")),
+        }
+    }
+}
+
+//Implementation of Dipslay trait for struct Question
+//Enables custom string formatting of Question instances
+//Displays the question struct in a formatted string
+impl std::fmt::Display for Question {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        write!(
+            f,
+            "{}, title: {}, content: {}, tags: {:?}",
+            self.id, self.title, self.content, self.tags
+        )
+    }
+}
+
+//Implementation of Display trait for struct QuestionId
+//Allows custom string formatting for QuestionId type
+//Outputs the id of the QuestionId (printed or logged)
+impl std::fmt::Display for QuestionId {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        write!(f, "id: {}", self.0)
+    }
+}
