b90f1fc25139a496d8c39532e6c5b9461a80f647
marvleon <123414280+marvleon@users.noreply.github.com>
2024-04-26 16:45:42 -0700

    Delete practice directory

--- practice/hello_cargo/target/debug/deps/hello_cargo.d
+++
@@ -1,5 +0,0 @@
-C:\Users\marvin\code\rust-cs410\Rust-Practice\practice\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
-
-C:\Users\marvin\code\rust-cs410\Rust-Practice\practice\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
-
-src\main.rs:
