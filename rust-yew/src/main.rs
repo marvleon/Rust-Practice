0f07984614b440e937455820be46573a9c8f5dee
marvleon <marvleon@pdx.edu>
2024-06-09 19:49:02 -0700

    add question insert functionality

--- rust-yew/src/main.rs
+++ rust-yew/src/main.rs
@@ -11,8 +11,81 @@ fn app() -> Html {
     let start = use_state(|| 0);
     let end = use_state(|| 1);
     let delete_id = use_state(|| "".to_string()); // State for deletion ID
+    let question_id = use_state(|| "".to_string());
+    let title = use_state(|| "".to_string());
+    let content = use_state(|| "".to_string());
+    let tags = use_state(Vec::new);
     
+    let on_id_change = {
+        let question_id = question_id.clone();
+        Callback::from(move |e: InputEvent| {
+            let input: web_sys::HtmlInputElement = e.target_unchecked_into();
+            question_id.set(input.value());
+        })
+    };
     
+    let on_title_change = {
+        let title = title.clone();
+        Callback::from(move |e: InputEvent| {
+            let input: web_sys::HtmlInputElement = e.target_unchecked_into();
+            title.set(input.value());
+        })
+    };
+    
+    let on_content_change = {
+        let content = content.clone();
+        Callback::from(move |e: InputEvent| {
+            let input: web_sys::HtmlInputElement = e.target_unchecked_into();
+            content.set(input.value());
+        })
+    };
+    
+    let on_tags_change = {
+        let tags = tags.clone();
+        Callback::from(move |e: InputEvent| {
+            let input: web_sys::HtmlInputElement = e.target_unchecked_into();
+            let tags_list = input.value().split(',').map(String::from).collect();
+            tags.set(tags_list);
+        })
+    };
+
+    let on_form_submit = {
+        let question_id = question_id.clone();
+        let title = title.clone();
+        let content = content.clone();
+        let tags = tags.clone();
+        Callback::from(move |e: FocusEvent| {
+            e.prevent_default();
+            let data = serde_json::json!({
+                "id": (*question_id).clone(),
+                "title": (*title).clone(),
+                "content": (*content).clone(),
+                "tags": (*tags).clone()
+            });
+            wasm_bindgen_futures::spawn_local(async move {
+                let url = "http://127.0.0.1:3030/add_question";
+                match Request::post(url)
+                    .header("Content-Type", "application/json")
+                    .body(data.to_string())
+                    .send()
+                    .await {
+                    Ok(response) => {
+                        if response.ok() {
+                            console::log_1(&"Question added successfully".into());
+                        } else {
+                            let status = response.status();
+                            let status_text = response.status_text();
+                            console::error_1(&format!("Failed to add question - Server responded with {}: {}", status, status_text).into());                        }
+                    }
+                    Err(err) => {
+                        console::error_1(&format!("Error sending request: {:?}", err).into());
+                    }
+                }
+            });
+        })
+    };
+    
+
     // Callback to handle delete ID input
     let on_delete_id_input = {
         let delete_id = delete_id.clone();
@@ -144,6 +217,15 @@ let on_click_paginate = {
         <div>
             <marquee>{ "WORK IN PROGRESS" }</marquee>
             <h1 style="text-align:center;">{ "Marvin's Rust Web App" }</h1>
+            <div>
+                <form onsubmit={on_form_submit}>
+                    <input type="text" placeholder="ID" oninput={on_id_change} />
+                    <input type="text" placeholder="Title" oninput={on_title_change} />
+                    <input type="text" placeholder="Content" oninput={on_content_change} />
+                    <input type="text" placeholder="Tags (comma-separated)" oninput={on_tags_change} />
+                    <button type="submit">{ "Add Question" }</button>
+                </form>
+            </div>
             <div>
                 <form onsubmit={on_delete_submit}>
                     <input type="text" placeholder="Enter ID to delete" oninput={on_delete_id_input} />
